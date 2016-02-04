1c35a234c1f48c96f0197e1cffd7a8f4faaa639a
Michael Walsh <Michaelwalsh223@hotmail.co.uk>
2016-02-04 14:06:00 +0000

    Tidied scripts and asset folder

---
+++ Assets/Scripts/vsMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class vsMove : MonoBehaviour {
+
+	float startTime;
+	
+	void Start () {
+		startTime = Time.time;
+	}
+	
+	void Update () {
+		if((Time.time - startTime) > 1.25f)
+		{
+			iTween.MoveTo(gameObject,iTween.Hash("y", 2.5f, "easeType", iTween.EaseType.easeInExpo, "time", 1));
+			iTween.ScaleTo(gameObject,iTween.Hash("x", 0, "y", 0, "z", 0, "easeType", iTween.EaseType.easeInExpo, "time", 1));
+		}
+		if(transform.localScale == Vector3.zero)
+			GameObject.Destroy(gameObject);
+	}
+}
